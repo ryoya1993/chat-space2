49429e9b7240ffeb255adfae46641991915b4b58
ryoya1993 <ryoya19930270@yahoo.co.jp>
2019-12-15 18:55:50 +0900

    グループ管理&ユーザ管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'groups#index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
 end
 
